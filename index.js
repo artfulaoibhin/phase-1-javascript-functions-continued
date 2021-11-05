747654ef139a5543a1ab0475085b3ea87b19426a
Eavan <eavan.nicholson@gmail.com>
2021-11-05 12:06:31 -0400

    commit tree 4081ef7661dc0ac247e6dc5711684881a246fe3c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate'){
+return (`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(task = 'go to the office'){
+return (`This Monday, I will ${task}.`)
+}
+
+function wrapAdjective(emphasis = '*'){
+    
+   return function emphatic(adjective = 'special'){
+        return `You are ${emphasis}${adjective}${emphasis}!`
+    }
+}
+
